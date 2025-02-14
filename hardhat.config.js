82a4f010fee253d025b2c0620c86586046e9e9fc
aamsetiana <aamsetiana27@gmail.com>
2025-02-14 13:48:48 +0700

    perbaikan bug

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,18 +5,6 @@ module.exports = {
   solidity: "0.8.28",
 };
 
-// require("@nomicfoundation/hardhat-toolbox");
-
-// module.exports = {
-//   solidity: "0.8.0",
-//   networks: {
-//     sepolia: {
-//       url: "https://eth-sepolia.alchemyapi.io/v2/QuAZV-0ETW0pNNZgGV8w5bAL1W5jFLLA",
-//       accounts: ["b16c9e2478f6b2f85218362b5e38a62c9f612a18bb6b570266ee6fcc7e653851"]
-//     }
-//   }
-// };
-
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
@@ -31,7 +19,7 @@ module.exports = {
   },
   networks: {
     sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/QuAZV-0ETW0pNNZgGV8w5bAL1W5jFLLA`,
+      url: `https://eth-sepolia.g.alchemy.com/v2/API_KEY_SEPOLIA_SAYA`,
       accounts: [
         "b16c9e2478f6b2f85218362b5e38a62c9f612a18bb6b570266ee6fcc7e653851",
       ],
